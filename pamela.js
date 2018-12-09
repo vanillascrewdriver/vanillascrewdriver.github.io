8057fb63e8c876c72a29c760c1442a2cd9f18918
vanillascrewdriver <psychfan99@gmail.com>
2018-12-09 00:03:43 -0500

    Update pamela.js

--- pamela.js
+++ pamela.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "HAPPY BIRTHDAY PAMELA!!!";
   }
 }, 1000);
